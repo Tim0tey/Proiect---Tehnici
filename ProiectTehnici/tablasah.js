91403b42ac3fdd768e005fa68ee9e4f4e04b9f93
leo <leonardo.bodan@emanuel.ro>
2025-06-01 21:32:27 +0300

    Working on Timers

--- ProiectTehnici/tablasah.js
+++ ProiectTehnici/tablasah.js
@@ -1,6 +1,32 @@
-function setup(){
+let whiteTime = 600;
+let blackTime = 600;
+let timerRunning = true;
+
+function setup() {
     createCanvas(cellSize * boardSize, cellSize * boardSize)
     initBoard();
+    tileSize = boardSize / 8;
+    setInterval(updateTimers, 1000);
+
+    pauseButton = createButton("Pauza");
+    pauseButton.mousePressed(() => timerRunning = !timerRunning);
+    pauseButton.position(540, 640);
+    pauseButton.style('padding', '10px');
+    pauseButton.style('background-color', 'grey');
+    pauseButton.style('color', 'white');
+    pauseButton.style('border', 'none');
+    pauseButton.style('border-radius', '8px');
+
+    reloadButton = createButton("Restart joc");
+    reloadButton.position(935, 640); // Poziționează butonul pe ecran
+    reloadButton.style('padding', '10px');
+    reloadButton.style('background-color', 'grey');
+    reloadButton.style('color', 'white');
+    reloadButton.style('border', 'none');
+    reloadButton.style('border-radius', '8px');
+    reloadButton.mousePressed(() => {
+        location.reload(); // Reîncarcă pagina
+    });
 }
 
 const boardSize = 8;
@@ -10,31 +36,31 @@ let board = [];
 const piese = {
     pawn: "",
     rook: "",
-    knight: "", 
+    knight: "",
     bishop: "",
     queen: "",
     king: "",
 }
-function draw(){
+function draw() {
     background(255);
     drawBoard();
     drawPieces();
     pieceMoves();
 }
-function initBoard(){
+function initBoard() {
     board = Array(boardSize).fill().map(() => Array(boardSize).fill(null));
 
     const ordonateBackPieces = ["rook", "knight", "bishop", "queen", "king", "bishop", "knight", "rook"];
 
     //acuma vom initializa ordonarea pieselor negre
-    for(let i = 0; i < boardSize; i++){
-        board[0][i] = { type: ordonateBackPieces[i], color: "black"}
-        board[1][i] = { type: "pawn", color: "black"}
+    for (let i = 0; i < boardSize; i++) {
+        board[0][i] = { type: ordonateBackPieces[i], color: "black" }
+        board[1][i] = { type: "pawn", color: "black" }
     }
 
     //ordonarea pieselor albe
-    for(let i = 0; i < boardSize; i++){
-        board[7][i] = { type: ordonateBackPieces[i], color: "white"}
-        board[6][i] = { type : "pawn", color: "white"}
+    for (let i = 0; i < boardSize; i++) {
+        board[7][i] = { type: ordonateBackPieces[i], color: "white" }
+        board[6][i] = { type: "pawn", color: "white" }
     }
 }
--- ProiectTehnici/timer.js
+++ ProiectTehnici/timer.js
@@ -1,5 +1,38 @@
+let timer;
+let timerInterval;
+let timerRunning = false;
+let button;
 
 function setup() {
     createCanvas(400, 400);
+    timer = 0;
+    button = createButton('Start/Stop Timer');
+    button.position(10, 420);
+    button.mousePressed(toggleTimer);
 }
 
+function draw() {
+    background(220);
+    textSize(32);
+    textAlign(CENTER, CENTER);
+    text('Timer: ' + timer, width / 2, height / 2);
+}
+
+function toggleTimer() {
+    if (timerRunning) {
+        stopTimer();
+    } else {
+        startTimer();
+    }
+    timerRunning = !timerRunning;
+}
+
+function startTimer() {
+    timerInterval = setInterval(() => {
+        timer++;
+    }, 1000);
+}
+
+function stopTimer() {
+    clearInterval(timerInterval);
+}
