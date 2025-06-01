e046ec899986669696896ce70e2fa2c8fd84c81d
Timotei <timotei.termure@emanuel.ro>
2025-06-01 20:47:46 +0300

    am uitat sa trimit o parte din commituri

--- ProiectTehnici/tablasah.js
+++ ProiectTehnici/tablasah.js
@@ -1,40 +1,120 @@
-function setup(){
+function setup() {
     createCanvas(cellSize * boardSize, cellSize * boardSize)
     initBoard();
 }
-
-const boardSize = 8;
-const cellSize = 80;
+let boardSize = 8;
+let cellSize = 80;
 let board = [];
+let selectedPiece = null;
+let turn = "white";
+let enPassantTarget = null;
+let gameMessage = "";
 
-const piese = {
-    pawn: "",
-    rook: "",
-    knight: "", 
-    bishop: "",
-    queen: "",
-    king: "",
-}
-function draw(){
+function draw() {
     background(255);
     drawBoard();
+    if (selectedPiece) {
+        highlightValidMoves(selectedPiece);
+    }
     drawPieces();
-    pieceMoves();
+    if(gameMessage !== ""){
+        fill(0);
+        textSize(24);
+        textAllign(screenLeft, TOP);
+        text(gameMessage, 10, 10)
+    }
 }
 function initBoard(){
-    board = Array(boardSize).fill().map(() => Array(boardSize).fill(null));
-
-    const ordonateBackPieces = ["rook", "knight", "bishop", "queen", "king", "bishop", "knight", "rook"];
-
-    //acuma vom initializa ordonarea pieselor negre
+    for(let y = 0; y < 8; y++){
+        board[y] = new Array(boardSize).fill(null);
+    }
+    const backRow = [Rook, Knight, Bishop, Queen, King, Bishop, Knight, Rook];
     for(let i = 0; i < boardSize; i++){
-        board[0][i] = { type: ordonateBackPieces[i], color: "black"}
-        board[1][i] = { type: "pawn", color: "black"}
+        board[0][i] = new backRow[i](i, 0, "black")
+        board[1][i] = new Pawn(i, 1, "black");
+        board[6][i] = new Pawn(i, 6, "white")
+        board[7][i] = new backRow[i](1,7, "white")
+    }
+}
+function mousePressed() {
+    const i = floor(mouseX / cellSize);
+    const j = floor(mouseY / cellSize);
+
+    if (i >= 0 && i < boardSize && j >= 0 && j < boardSize) {
+        clickOnBoard(i, j);
+    }
+}
+function clickOnBoard(x, y) {
+    const piece = board[y][x];
+    if (selectedPiece) {
+        moveSelectedPiecteto(x, y);
+    }else if(piece && piece.color === turn){
+        selectedPiece(piece);
+    }
+}
+function selectedPiece(piece){
+    selectedPiece = piece;
+}
+function moveSelectedPiecteto(x, y){
+    if(!selectedPiece.canMove(x, y, board)){
+        selectedPiece = null;
+        return;
     }
 
-    //ordonarea pieselor albe
-    for(let i = 0; i < boardSize; i++){
-        board[7][i] = { type: ordonateBackPieces[i], color: "white"}
-        board[6][i] = { type : "pawn", color: "white"}
+    specialMove(x, y)
+
+    board[selectedPiece.y][selectedPiece.x] = null;
+    board[y][x] = selectedPiece;
+    selectedPiece.x = x;
+    selectedPiece.y = y;
+
+    selectedPiece = null;
+    switchTurn();
+
+    if(!isKingAlive(board, turn)){
+        noLoop();
+        if(turn === "white"){
+            gameMessage = "Black wins!";
+        } else {
+            gameMessage = "You win!"
+        }
+    }else if(checkDrawByInsufficientMaterial(board)){
+        noLoop();
+
     }
+}
+function specialMove(){
+    if(){
+
+    }
+}
+
+function isKingAlive(){
+    for(let row of board){
+        for(let piece of row){
+            if(piece && piece instanceof King && piece.color === color)
+                return true;
+        }
+    }
+    return false;
+}
+function checkDrawByInsufficientMaterial(){
+    const pieces = [];
+    for(let row of board){
+        for(let piece of row){
+            if(piece){
+                pieces.push(piece);
+            }
+        }
+    }
+    if(pieces.length === 2){
+        return true;
+    }
+    if(pieces.length === 3){
+        const minor = pieces.find(p => !(p instanceof King));
+        if(minor instanceof Bishop || minor instanceof Knight){
+            return true;
+        }
+    }
+    return false;
 }
